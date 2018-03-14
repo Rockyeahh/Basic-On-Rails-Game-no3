21213de1162b02d745f1dea02abef43bc1de3f60
Soren's Coding Land <chaz.com@hotmail.co.uk>
2018-03-14 18:54:32 +0000

    Basic On Rails Game no 3 - I made the camera move to where it needs to go and the end goal is set up.
    The UI button for going to the score screen is not finished and doesn't set active.
    Also the on trigger doesn't make the camera stop moving.

---
+++ Basic 3D On Rails Game no 3 project/Basic 3D On Rails Game no 3/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    public int speed;
+
+	void Start () {
+        //transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+	
+	void Update () {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
---
+++ Basic 3D On Rails Game no 3 project/Basic 3D On Rails Game no 3/Assets/Scripts/EndGoal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndGoal : MonoBehaviour {
+
+    public GameObject ScoreScreen;
+
+    void Awake()
+    {
+        // Screen Screen On disable?
+    }
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Make UI score screen button on enable.
+        ScoreScreen.SetActive(true);
+    }
+
+}
